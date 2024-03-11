f7b1eb522f10a9427372b5e1ad5676b8c95f1230
thaidui123 <vothongthai02012006@gmail.com>
2024-03-11 08:39:33 +0700

    Add project files.

---
+++ LTCB_WHILE numbersum1/LTCB_WHILE numbersum1.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cout << "nhap chieu cao tam giac:";
+	cin >> n;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = 1; j <= 1; ++j) {
+			cout << "*";
+		}
+	}
+
+
+
+	cout << endl;
+	return 0;
+}
+
